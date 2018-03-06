7a902de7864c28b2f1736d3f02a06f1db2083dde
Malte Hübner <maltehuebner@gmx.org>
2018-03-06 15:44:29 +0100

    Remove old stuff from CityEntity.

--- src/Limitation/Entity/City.php
+++ src/Limitation/Entity/City.php
@@ -1,62 +1,14 @@
 <?php declare(strict_types=1);
 
-namespace App\Entity;
+namespace App\Limitation\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-
-/**
- * @ORM\Entity(repositoryClass="App\Repository\CityRepository")
- */
 class City
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\Column(type="string")
-     */
+    /** @var string $name */
     private $name;
 
-    /**
-     * @ORM\Column(type="string")
-     */
-    private $slug;
-
-    /**
-     * @ORM\Column(type="text")
-     */
+    /** @var string $desription */
     private $desription;
-
-    /**
-     * @ORM\Column(type="float")
-     */
-    private $latitude;
-
-    /**
-     * @ORM\Column(type="float")
-     */
-    private $longitude;
-
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $createdAt;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function setId(int $id): City
-    {
-        $this->id = $id;
-
-        return $this;
-    }
 
     public function getName(): ?string
     {
@@ -70,18 +22,6 @@ class City
         return $this;
     }
 
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
-
-    public function setSlug(string $slug): City
-    {
-        $this->slug = $slug;
-
-        return $this;
-    }
-
     public function getDescription(): ?string
     {
         return $this->desription;
@@ -90,42 +30,6 @@ class City
     public function setDescription(string $description): City
     {
         $this->desription = $description;
-
-        return $this;
-    }
-
-    public function getLatitude(): ?float
-    {
-        return $this->latitude;
-    }
-
-    public function setLatitude(float $latitude): City
-    {
-        $this->latitude = $latitude;
-
-        return $this;
-    }
-
-    public function getLongitude(): ?float
-    {
-        return $this->longitude;
-    }
-
-    public function setLongitude(float $longitude): City
-    {
-        $this->longitude = $longitude;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTime
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTime $createdAt): City
-    {
-        $this->createdAt = $createdAt;
 
         return $this;
     }
