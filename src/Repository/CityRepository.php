898b38b9d2cb6908c9c4ea09d3a090f0bb064423
Malte Hübner <maltehuebner@gmx.org>
2018-03-06 14:11:48 +0100

    Auto generated city entity and repository.

---
+++ src/Repository/CityRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\City;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method City|null find($id, $lockMode = null, $lockVersion = null)
+ * @method City|null findOneBy(array $criteria, array $orderBy = null)
+ * @method City[]    findAll()
+ * @method City[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CityRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, City::class);
+    }
+
+    /*
+    public function findBySomething($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->where('c.something = :value')->setParameter('value', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+}
