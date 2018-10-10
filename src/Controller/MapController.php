87dee146f8fc3367d7a9cade7cb7a32fb3da3d02
Malte Hübner <maltehuebner@gmx.org>
2018-10-10 19:01:09 +0200

    Totally cache everything.

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,25 +2,38 @@
 
 namespace App\Controller;
 
+use App\Limitation\Entity\City;
 use App\Limitation\Parser\GeoJsonParser;
+use Psr\SimpleCache\CacheInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class MapController extends Controller
 {
-    public function city(GeoJsonParser $parser, string $citySlug): Response
+    public function city(CacheInterface $cache, GeoJsonParser $parser, string $citySlug): Response
     {
-        $geoJsonUrl = sprintf('https://raw.githubusercontent.com/maltehuebner/fahrverbote/master/%s.geojson', $citySlug);
+        $city = $this->getCityFromCache($cache, $citySlug);
 
-        if (false === @file($geoJsonUrl)) {
+        if (!$city) {
             throw $this->createNotFoundException(sprintf('City %s not found', $citySlug));
         }
 
-        $parser->loadFromFile($geoJsonUrl)->parse();
+        $geoJsonUrl = sprintf('https://raw.githubusercontent.com/maltehuebner/fahrverbote/master/%s.geojson', $citySlug);
 
         return $this->render('map/city.html.twig', [
             'geoJsonUrl' => $geoJsonUrl,
-            'city' => $parser->getCity(),
+            'city' => $city,
         ]);
+    }
+
+    protected function getCityFromCache(CacheInterface $cache, string $citySlug): ?City
+    {
+        $cities = $cache->get('cities');
+
+        if (!is_array($cities) || !array_key_exists($citySlug, $cities)) {
+            return null;
+        }
+
+        return $cities[$citySlug];
     }
 }
